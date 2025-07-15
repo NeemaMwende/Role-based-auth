bf4e26a8676ab1d47fb327c3c790acf8d4644cc8
NeemaMwende <gracepete12@gmail.com>
2025-07-15 14:43:57 +0300

    admin configuration

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, DoctorProfile, PatientProfile, NurseProfile
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'role', 'first_name', 'last_name', 'is_active']
+    list_filter = ['role', 'is_active', 'date_joined']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('role', 'phone_number', 'date_of_birth', 'address')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(DoctorProfile)
+admin.site.register(PatientProfile)
+admin.site.register(NurseProfile)
